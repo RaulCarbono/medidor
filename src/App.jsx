ca191bc54f97d6d5d82b7091bea6d3d4676f5e42
luismaviit <luismaviit@gmail.com>
2023-01-24 14:18:02 -0500

    add sideBar MUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import Principal from "./components/Principal";
 import Sidebar from "./components/Sidebar";
 import Cards from "./components/Cards";
+import SideBar from "./components/newCom/sideBar"
 
 function App() {
   return (
     <>
-      <Principal />
+      <SideBar />
     </>
   );
 }
